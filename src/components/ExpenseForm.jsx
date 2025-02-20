12c138a2265b135957246f15cd422d5280303465
raushan <kraushan022@gmail.com>
2025-02-20 22:44:15 +0530

    add form add functionality

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ setExpenses }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(getFormData(e.target));
+    setExpenses((prevData) => [...prevData, {...getFormData(e.target), id: crypto.randomUUID()}]);
+    e.target.reset()
+  };
+
+  const getFormData = (form) => {
+    const formData = new FormData(form);
+    const data = {};
+    for (const [key, value] of formData.entries()) {
+      data[key] = value;
+    }
+    return data;
+  };
+
   return (
-    <form className="expense-form">
+    <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" />
+        <input id="title" name="title" />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <input id="category" />
+        <select id="category" name="category">
+          <option value="" hidden>
+            Select Category
+          </option>
+          <option value="grocery">Grocery</option>
+          <option value="clothes">Clothes</option>
+          <option value="bills">Bills</option>
+          <option value="education">Education</option>
+          <option value="medicine">Medicine</option>
+        </select>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" />
+        <input id="amount" name="amount" />
       </div>
       <button className="add-btn">Add</button>
     </form>
