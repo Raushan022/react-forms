3c4b411ae15b7933828367376c1b7179c492606c
raushan <kraushan022@gmail.com>
2025-02-26 18:41:27 +0530

    add onContextMenu

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-const ContextMenu = () => {
+const ContextMenu = ({menuPosition, setMenuPosition}) => {
+  // if (!menuPosition.left) return
   return (
-    <div className="context-menu">
-      <div>Edit</div>
-      <div>Delete</div>
+    <div className="context-menu" style={menuPosition}>
+      <div
+      onClick={()=> {
+        console.log('editing')
+        setMenuPosition({})
+      }}
+      >Edit</div>
+      <div
+      onClick={() => {
+        console.log('deleting')
+        setMenuPosition({})
+      }}
+      >Delete</div>
     </div>
   );
 };
